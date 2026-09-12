5188d8869006fea89627971cac3418b2d021f141
Baran <325435635+barannnnnnnnn@users.noreply.github.com>
2026-09-12 17:06:00 +0300

    home

---
+++ menuData/promotions.ts
@@ -0,0 +1,15 @@
+export type PromotionsData = {
+  image: string;
+  link: string;
+};
+
+export const promotions: PromotionsData[] = [
+  {
+    image: "src/assets/promotions/promotion.png",
+    link: "",
+  },
+  {
+    image: "src/assets/promotions/promotion2.png",
+    link: "",
+  },
+];
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
-import FoodCard from "../components/FoodCard";
 import PromotionCarousel from "../components/PromotionCarousel";
+import { promotions } from "../../menuData/promotions";
 
 import { CarouselItem } from "../../@/components/ui/carousel";
+import MenuCategoryCard from "../components/MenuCategoryCard";
+import { categories } from "../../menuData/categories";
 
 function Home() {
   return (
@@ -9,92 +11,43 @@ function Home() {
       <div className="flex justify-center lg:pl-[5px] lg:pr-[5px] lg:mt-[10px] mb-[10px]">
         <div className="max-w-[1200px]">
           <PromotionCarousel>
-            <CarouselItem>
-              <a href="" className="flex w-full h-full ">
-                <img
-                  src="src/assets/promotions/promotion.png"
-                  alt=""
-                  className="flex justify-center align-middle items-center w-full h-full lg:rounded-[15px]"
-                />
-              </a>
-            </CarouselItem>
-            <CarouselItem>
-              <a href="" className="flex w-full h-full ">
-                <img
-                  src="src/assets/promotions/promotion2.png"
-                  alt=""
-                  className="flex justify-center align-middle items-center w-full h-full lg:rounded-[15px]"
-                />
-              </a>
-            </CarouselItem>
+            {promotions.map((promotions, index) => (
+              <CarouselItem key={index}>
+                <a href={promotions.link} className="flex w-full h-full ">
+                  <img
+                    src={promotions.image}
+                    alt=""
+                    className="flex justify-center align-middle items-center w-full h-full lg:rounded-[15px]"
+                  />
+                </a>
+              </CarouselItem>
+            ))}
           </PromotionCarousel>
 
-          <p className="text-center font-semibold text-[24px] mt-[15px]">
-            Döner Wraps
-          </p>
-          <div className="bg-[#ff5a1e] h-[2px] mb-[5px]"></div>
-          <div className="flex flex-wrap justify-center gap-[5px]">
-            <FoodCard
-              name={"Döner Wrap"}
-              price={"11,99€"}
-              image={"src/assets/products/doner-wrap.png"}
-              product={""}
-            ></FoodCard>
-            <FoodCard
-              name={"Döner Wrap 2"}
-              price={"7,99€"}
-              oldPrice={"10,99€"}
-              image={"src/assets/products/doner-wrap2.png"}
-              product={""}
-            ></FoodCard>
-          </div>
-
-          <p className="text-center font-semibold text-[24px] mt-[15px]">
-            Fries
-          </p>
-          <div className="bg-[#ff5a1e] h-[2px] mb-[5px]"></div>
-          <div className="flex flex-wrap justify-center gap-[5px]">
-            <FoodCard
-              name={"Fries"}
-              price={"2,99€"}
-              image={"src/assets/products/fries.png"}
-              product={""}
-            ></FoodCard>
-          </div>
-
-          <p className="text-center font-semibold text-[24px] mt-[15px]">
-            Desserts
-          </p>
-          <div className="bg-[#ff5a1e] h-[2px] mb-[5px]"></div>
-          <div className="flex flex-wrap justify-center gap-[5px]">
-            <FoodCard
-              name={"Baklava (100g)"}
-              price={"2,99€"}
-              image={"src/assets/products/baklava.png"}
-              product={""}
-            ></FoodCard>
-          </div>
-
-          <p className="text-center font-semibold text-[24px] mt-[15px]">
-            Drinks
-          </p>
-          <div className="bg-[#ff5a1e] h-[2px] mb-[5px]"></div>
-          <div className="flex flex-wrap justify-center gap-[5px]">
-            <FoodCard
-              name={"Ayran (250 ml)"}
-              price={"1,99€"}
-              image={"src/assets/products/ayran.png"}
-              product={""}
-            ></FoodCard>
-          </div>
-
-          <div className="w-full flex justify-center m-[10px]">
-            <a
-              href="/menu"
-              className="hover:bg-[#ff5a1e] hover:text-white p-[5px] pl-[15px] pr-[15px] border-[#ff5a1e] border-2 rounded-[10px]"
-            >
-              View Menu
-            </a>
+          <div className="mt-[10px]">
+            <p className="text-center font-semibold text-[24px]">
+              What Would You Like?
+            </p>
+            <div className="bg-[#ff5a1e] h-[5px]"></div>
+            <div className="lg:flex justify-center mt-[10px]">
+              <div className="grid lg:grid-cols-4 gap-[15px]">
+                {categories.map((categories) => (
+                  <MenuCategoryCard
+                    category={categories.name}
+                    image={categories.image}
+                    link={`/menu/${categories.slug}`}
+                  />
+                ))}
+              </div>
+            </div>
+            <div className="w-full flex justify-center m-[10px]">
+              <a
+                href="/menu"
+                className="hover:bg-[#ff5a1e] hover:text-white p-[5px] pl-[15px] pr-[15px] border-[#ff5a1e] border-2 rounded-[10px]"
+              >
+                View Menu
+              </a>
+            </div>
           </div>
         </div>
       </div>
